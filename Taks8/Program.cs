32487de4521fe831f112f65e915925e2d689bf10
Galya <mazur.galusay@gmail.com>
2022-11-30 20:27:51 +0300

    третья задача из домашнего задания

---
+++ Taks8/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая на входе принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите целое число ");
+string num1 = Console.ReadLine()!;
+int num1int = int.Parse(num1);
+
+if (int i = 1 ; i <= num1int ; i++)
+{
+    if (num1 % 2 == 0)
+    {
+        Console.WriteLine(num1 + "четное число");
+    }
+}
