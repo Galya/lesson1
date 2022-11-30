e404ca6ac3e28f6173df301640d5ffbf7e6c1cc6
Galya <mazur.galusay@gmail.com>
2022-11-30 20:27:22 +0300

    вторая задача из домашнего задания

---
+++ Taks6/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу где на входе принмает число и выдает, является ли число четным
+
+Console.WriteLine("Введите целое число ");
+string input1 = Console.ReadLine()!;
+int input1int = int.Parse(input1);
+
+if (input1int % 2 == 0)
+{
+    Console.WriteLine(input1 + "четное число");
+}
+else
+{
+    Console.WriteLine(input1 + "не четное число");
+}
