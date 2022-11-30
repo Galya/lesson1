c129ac8080dd7e0770b303fb598a847093aa6363
Galya <mazur.galusay@gmail.com>
2022-11-30 20:26:14 +0300

    первая задача из домашнего задания

---
+++ taks2/Program.cs
@@ -0,0 +1,24 @@
+﻿// напишите программу, которая на входе принимает два числа и выдает, какое число больше, а какое меньше
+
+Console.WriteLine("Введите первое число ");
+string input1 = Console.ReadLine()!;
+int input1int = int.Parse(input1);
+
+Console.WriteLine("Введите второе число ");
+string input2 = Console.ReadLine()!;
+int input2int = int.Parse(input2);
+
+if (input1int > input2int)
+{
+    Console.WriteLine(input1int + "Больше" + input2int);
+}
+else if (input2int >input1int)
+{
+    Console.WriteLine(input2int + "Больше" + input1int);
+}
+else
+{
+    Console.WriteLine("числа равны");
+}
+
+
